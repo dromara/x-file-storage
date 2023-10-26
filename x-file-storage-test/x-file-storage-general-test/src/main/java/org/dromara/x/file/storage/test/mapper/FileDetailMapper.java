9ee1fd63bae3b2723f4dd1e71aa1eec8baf1e76c
wanghaiqi <wanghaiqi@beeplay123.com>
2023-10-26 11:23:01 +0800

    :zap: Commit content: - 重新命名测试模块名称，名称错误

--- x-file-storage-test/x-file-storage-general-test/src/main/java/org/dromara/x/file/storage/test/mapper/FileDetailMapper.java
+++
@@ -1,7 +0,0 @@
-package org.dromara.x.file.storage.test.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.dromara.x.file.storage.test.model.FileDetail;
-
-public interface FileDetailMapper extends BaseMapper<FileDetail> {
-}
