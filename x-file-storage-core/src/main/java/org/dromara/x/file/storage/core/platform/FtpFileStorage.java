7772b69e1ffc794134042c62f8150f55e9eca137
xuyanwu <1171736840@qq.com>
2023-11-11 10:11:31 +0800

    Update:适配 FTP 移动文件功能

--- x-file-storage-core/src/main/java/org/dromara/x/file/storage/core/platform/FtpFileStorage.java
+++ x-file-storage-core/src/main/java/org/dromara/x/file/storage/core/platform/FtpFileStorage.java
@@ -2,21 +2,28 @@ package org.dromara.x.file.storage.core.platform;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.io.IORuntimeException;
+import cn.hutool.core.io.file.FileNameUtil;
+import cn.hutool.core.io.file.PathUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.ftp.Ftp;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.function.Consumer;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.dromara.x.file.storage.core.FileInfo;
 import org.dromara.x.file.storage.core.FileStorageProperties.FtpConfig;
 import org.dromara.x.file.storage.core.InputStreamPlus;
+import org.dromara.x.file.storage.core.ProgressListener;
 import org.dromara.x.file.storage.core.UploadPretreatment;
 import org.dromara.x.file.storage.core.exception.FileStorageRuntimeException;
+import org.dromara.x.file.storage.core.move.MovePretreatment;
 
 /**
  * FTP 存储
@@ -185,6 +192,89 @@ public class FtpFileStorage implements FileStorage {
             }
         } catch (IOException | IORuntimeException e) {
             throw new FileStorageRuntimeException("缩略图文件下载失败！fileInfo：" + fileInfo, e);
+        } finally {
+            returnClient(client);
+        }
+    }
+
+    @Override
+    public boolean isSupportSameMove() {
+        return true;
+    }
+
+    @Override
+    public void sameMove(FileInfo srcFileInfo, FileInfo destFileInfo, MovePretreatment pre) {
+        if (srcFileInfo.getFileAcl() != null && pre.getNotSupportAclThrowException()) {
+            throw new FileStorageRuntimeException(
+                    "文件移动失败，不支持设置 ACL！srcFileInfo：" + srcFileInfo + "，destFileInfo：" + destFileInfo);
+        }
+        if (CollUtil.isNotEmpty(srcFileInfo.getUserMetadata()) && pre.getNotSupportMetadataThrowException()) {
+            throw new FileStorageRuntimeException(
+                    "文件移动失败，不支持设置 Metadata！srcFileInfo：" + srcFileInfo + "，destFileInfo：" + destFileInfo);
+        }
+        if (!basePath.equals(srcFileInfo.getBasePath())) {
+            throw new FileStorageRuntimeException("文件移动失败，源文件 basePath 与当前存储平台 " + platform + " 的 basePath " + basePath
+                    + " 不同！srcFileInfo：" + srcFileInfo + "，destFileInfo：" + destFileInfo);
+        }
+
+        String srcPath = getAbsolutePath(srcFileInfo.getBasePath() + srcFileInfo.getPath());
+        String destPath = getAbsolutePath(destFileInfo.getBasePath() + destFileInfo.getPath());
+        String relativizePath =
+                Paths.get(srcPath).relativize(Paths.get(destPath)).toString().replace("\\", "/") + "/";
+
+        Ftp client = getClient();
+        try {
+            FTPClient ftpClient = client.getClient();
+            client.cd(srcPath);
+
+            FTPFile srcFile;
+            try {
+                srcFile = ftpClient.listFiles(srcFileInfo.getFilename())[0];
+            } catch (Exception e) {
+                throw new FileStorageRuntimeException(
+                        "文件移动失败，无法获取源文件信息！srcFileInfo：" + srcFileInfo + "，destFileInfo：" + destFileInfo, e);
+            }
+
+            // 移动缩略图文件
+            String destThFileRelativizeKey = null;
+            if (StrUtil.isNotBlank(srcFileInfo.getThFilename())) {
+                destFileInfo.setThUrl(domain + getThFileKey(destFileInfo));
+                destThFileRelativizeKey = relativizePath + destFileInfo.getThFilename();
+                try {
+                    client.mkDirs(destPath);
+                    ftpClient.rename(srcFileInfo.getThFilename(), destThFileRelativizeKey);
+                } catch (Exception e) {
+                    throw new FileStorageRuntimeException(
+                            "缩略图文件移动失败！srcFileInfo：" + srcFileInfo + "，destFileInfo：" + destFileInfo);
+                }
+            }
+
+            // 移动文件
+            String destFileKey = getFileKey(destFileInfo);
+            destFileInfo.setUrl(domain + destFileKey);
+            String destFileRelativizeKey = relativizePath + destFileInfo.getFilename();
+            try {
+                ProgressListener.quickStart(pre.getProgressListener(), srcFile.getSize());
+                ftpClient.rename(srcFileInfo.getFilename(), destFileRelativizeKey);
+                ProgressListener.quickFinish(pre.getProgressListener(), srcFile.getSize());
+            } catch (Exception e) {
+                if (destThFileRelativizeKey != null) {
+                    try {
+                        ftpClient.rename(destThFileRelativizeKey, srcFileInfo.getThFilename());
+                    } catch (Exception ignored) {
+                    }
+                }
+                try {
+                    if (client.existFile(srcFileInfo.getFilename())) {
+                        client.delFile(destFileRelativizeKey);
+                    } else {
+                        ftpClient.rename(destFileRelativizeKey, srcFileInfo.getFilename());
+                    }
+                } catch (Exception ignored) {
+                }
+                throw new FileStorageRuntimeException(
+                        "文件移动失败！srcFileInfo：" + srcFileInfo + "，destFileInfo：" + destFileInfo);
+            }
         } finally {
             returnClient(client);
         }
